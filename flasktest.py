8c2a40ac4f8f268e9db5c9797e9aad88c60a7724
Ali <alikhan37544@gmail.com>
2023-12-21 20:19:00 +0530

    basic thing

---
+++ flasktest.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return "The website should be working right now :)"
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
